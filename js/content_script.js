36354cb316ecb224ea08530029857e8659ffe9a1
mengodkidjo <mengodkidjo@gmail.com>
2024-06-12 11:11:03 +0100

    first commit

---
+++ js/content_script.js
@@ -0,0 +1,5 @@
+
+    // let answerDivs = document.getElementsByClassName("answer js-answer accepted-answer js-accepted-answer")
+    // let answerDiv = answerDivs[0];
+    // answerDiv.style.backgroundColor = "red";
+    document.body.style.border = "5px solid red";
